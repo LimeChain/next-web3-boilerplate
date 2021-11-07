335bd1d38f8dac9a5e67ac28a95e61709c8db236
ochikov <ognyan@limechain.tech>
2021-11-07 21:42:07 +0200

    initial commit

---
+++ constants/index.ts
@@ -0,0 +1,14 @@
+
+  export interface Networks {
+    [key: number]: string;
+  }
+  export const walletConnectSupportedNetworks: Networks = {
+    // Add your network rpc URL here
+    1: "https://ethereumnode.defiterm.io",
+    3: "https://ethereumnode.defiterm-dev.net"
+  };
+
+  // Network chain ids
+  export const supportedMetamaskNetworks = [1, 3, 4, 5, 42];
+
+  export const ALBT_TOKEN_ADDRESS = "0xc6869a93ef55e1d8ec8fdcda89c9d93616cf0a72";
---
+++ pages/index.tsx
@@ -0,0 +1,65 @@
+import { useWeb3React } from "@web3-react/core";
+import Head from "next/head";
+import Link from "next/link";
+import Account from "../components/Account";
+import NativeCurrencyBalance from "../components/NativeCurrencyBalance";
+import TokenBalance from "../components/TokenBalance";
+import { ALBT_TOKEN_ADDRESS } from "../constants";
+import useEagerConnect from "../hooks/useEagerConnect";
+
+function Home() {
+  const { account, library } = useWeb3React();
+
+  const triedToEagerConnect = useEagerConnect();
+
+  const isConnected = typeof account === "string" && !!library;
+
+  return (
+    <div>
+      <Head>
+        <title>next-web3-boilerplate</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <header>
+        <nav>
+          <Link href="/">
+            <a>next-web3-boilerplate</a>
+          </Link>
+
+          <Account triedToEagerConnect={triedToEagerConnect} />
+        </nav>
+      </header>
+
+      <main>
+        <h1>
+          Welcome to{" "}
+          <a href="https://github.com/mirshko/next-web3-boilerplate">
+            next-web3-boilerplate
+          </a>
+        </h1>
+
+        {isConnected && (
+          <section>
+            <NativeCurrencyBalance />
+
+            <TokenBalance tokenAddress={ALBT_TOKEN_ADDRESS} symbol="ALBT" />
+          </section>
+        )}
+      </main>
+
+      <style jsx>{`
+        nav {
+          display: flex;
+          justify-content: space-between;
+        }
+
+        main {
+          text-align: center;
+        }
+      `}</style>
+    </div>
+  );
+}
+
+export default Home;
